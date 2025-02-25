1bc1e26c8127b66ad22135dd77fdeb29e2068433
lolesterrr <kajjabwangulester@gmail.com>
2025-02-25 15:04:18 +0300

    several changes to welcome page also changed several foldernames to approporiate ones and added sign up page skeleton

--- Frontend/aits_frontend/src/App.js
+++ Frontend/aits_frontend/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Welcomepage from './AITS_Pages/Welcomepage';
+import WelcomePage from './AITS_Pages/WelcomePage';
+import SignUpPage from './AITS_Pages/SignUpPage';
+// import LoginPage from './AITS_Pages/LoginPage';
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Welcomepage />} />
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        {/* <Route path="/" element={<LoginPage />} /> */}
       </Routes>
     </Router>
   );
