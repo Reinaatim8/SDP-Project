ced28625efdf73b98db2fe17923ffea2be51b9dc
lolesterrr <kajjabwangulester@gmail.com>
2025-02-26 12:57:06 +0300

    comment out login code from App.js

--- Frontend/aits_frontend/src/App.js
+++ Frontend/aits_frontend/src/App.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-<<<<<<< HEAD
 import WelcomePage from './AITS_Pages/WelcomePage';
 import SignUpPage from './AITS_Pages/SignUpPage';
 // import LoginPage from './AITS_Pages/LoginPage';
 
-=======
 import Welcomepage from './AITS_Pages/Welcomepage';
-import Loginpage from './AITS_Pages/Login page';
->>>>>>> 737e0a7312c54a2ad8e256250932c2de2943d7ef
+// import Loginpage from './AITS_Pages/Login page';
 
 function App() {
   return (
     <Router>
       <Routes>
-<<<<<<< HEAD
         <Route path="/" element={<WelcomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
         {/* <Route path="/" element={<LoginPage />} /> */}
 =======
         <Route path="/" element={<Welcomepage />} />
         <Route path="/" element={<Loginpage />} />
->>>>>>> 737e0a7312c54a2ad8e256250932c2de2943d7ef
       </Routes>
     </Router>
   );
