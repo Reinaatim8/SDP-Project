57832ab456d2ae21ff26000272669ae6ffa2dcd8
Reinaatim8 <168911378+Reinaatim8@users.noreply.github.com>
2025-02-26 04:42:10 +0300

    creared student_dashboard

--- Frontend/aits_frontend/src/App.js
+++ Frontend/aits_frontend/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Welcomepage from './AITS_Pages/Welcomepage';
-import Loginpage from './AITS_Pages/Login page';
+import StudentDashboard from './AITS_Pages/Student_dashbaord';
+
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Welcomepage />} />
-        <Route path="/" element={<Loginpage />} />
+        
+        <Route path="/" element={<StudentDashboard/>}/>
       </Routes>
     </Router>
   );
