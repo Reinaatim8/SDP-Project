036fa13d777d67ac709272201e9138c71fa99c5b
lolesterrr <kajjabwangulester@gmail.com>
2025-02-27 21:35:19 +0300

    changes to dashboard

--- Frontend/aits_frontend/src/App.js
+++ Frontend/aits_frontend/src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WelcomePage from './AITS_Pages/WelcomePage';
 import SignUpPage from './AITS_Pages/SignUpPage';
-// import LoginPage from './AITS_Pages/LoginPage';
-
+import StudentDashboard from './AITS_Pages/StudentDashboard'; // ✅ Import the dashboard
 
 function App() {
   return (
@@ -11,9 +10,10 @@ function App() {
       <Routes>
         <Route path="/" element={<WelcomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
-        {/* <Route path="/" element={<LoginPage />} /> */}
+        <Route path="/dashboard" element={<StudentDashboard />} /> {/* ✅ Add dashboard route */}
       </Routes>
     </Router>
   );
 }
+
 export default App;
