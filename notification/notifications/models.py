33d0c35cd1d49dc950ad0d43d1e0511616a7b7d9
Spire256 <mwanjesimonpeter732@gmail.com>
2025-03-31 22:53:44 +0300

    Added something to models.py

--- notification/notifications/models.py
+++ notification/notifications/models.py
@@ -31,6 +31,8 @@ class Notification(models.Model):
     def __str__(self):
         return f"{self.notification_type} notification for {self.recipient.email}"
 
+
+#Logs email notification delivery status
 class EmailNotificationLog(models.Model):
     notification = models.ForeignKey(Notification, on_delete=models.CASCADE)
     sent_at = models.DateTimeField(default=timezone.now)
