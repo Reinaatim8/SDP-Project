92caf6ac0bca2eaaf520b0cfc8384753f3702532
Spire256 <mwanjesimonpeter732@gmail.com>
2025-03-31 22:57:14 +0300

    Added something to serializers.py

--- notification/notifications/serializers.py
+++ notification/notifications/serializers.py
@@ -9,7 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'first_name', 'last_name']
-
+#Serializes notification data for API
 class NotificationSerializer(serializers.ModelSerializer):
     recipient = UserSerializer(read_only=True)
     sender = UserSerializer(read_only=True)
