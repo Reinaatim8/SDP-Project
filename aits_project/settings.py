7fb0e495c23b722a8e0051fe2f90afa97b6f447f
kajjabwangu lester <kajjabwangulester@gmail.com>
2025-04-03 16:06:29 +0300

    Update settings.py

--- aits_project/settings.py
+++ aits_project/settings.py
@@ -196,6 +196,7 @@ SPECTACULAR_SETTINGS={
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://aits-space.vercel.app/",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
