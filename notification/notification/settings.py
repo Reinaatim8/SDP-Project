e7a943b51f39b862ba134aa567d3dc0124adb1c6
Spire256 <mwanjesimonpeter732@gmail.com>
2025-04-06 16:39:30 +0300

    Added something

--- notification/notification/settings.py
+++ notification/notification/settings.py
@@ -154,7 +154,7 @@ REST_FRAMEWORK = {
 
 
 from datetime import timedelta
-
+#JWT token lifetime settingss
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
