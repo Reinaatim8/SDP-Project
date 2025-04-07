8edc9962eb90d4aa3384eba5505a5bb836c1de83
Kennedy Mutebi <kennedymutebi7@gmail.com>
2025-04-07 02:49:58 -0700

    added

--- Backend/apps.py
+++ Backend/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class IssuesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.issues'
+    name = 'apps.issues'   
+    # gg
