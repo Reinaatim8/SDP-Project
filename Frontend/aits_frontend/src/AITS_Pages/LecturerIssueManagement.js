7e15fd03ae39acbb61bbed30d3146f39ca3873a7
lolesterrr <kajjabwangulester@gmail.com>
2025-04-17 23:47:58 +0300

    partial warning fix

--- Frontend/aits_frontend/src/AITS_Pages/LecturerIssueManagement.js
+++ Frontend/aits_frontend/src/AITS_Pages/LecturerIssueManagement.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { FaUserCircle, FaFilter, FaSearch, FaComments, FaEdit, FaCheck } from "react-icons/fa";
+import { FaUserCircle,  FaSearch, FaComments, FaEdit, } from "react-icons/fa";
 import axios from "axios";
 import LecturerHoverBar from "../components/LecturerHoverBar";
 import "./LecturerIssueManagement.css";
