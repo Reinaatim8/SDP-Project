483f5386ea1bcfc35a5d345d1661f640b3cc3567
Kennedy Mutebi <kennedymutebi7@gmail.com>
2025-04-23 12:07:50 +0300

    debug in urls

--- Backend/urls.py
+++ Backend/urls.py
@@ -7,7 +7,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 router = DefaultRouter()
 router.register(r'users', views.UserViewSet, basename="users")
 router.register(r'courses', views.CourseViewSet, basename="courses")
-router.register(r'enrollments', views.EnrollmentViewSet)
+router.register(r'enrollments', views.EnrollmentViewSet, basename="enrollments")
 router.register(r'categories', views.IssueCategoryViewSet)
 router.register(r'issues', views.IssueViewSet)
 router.register(r'comments', views.CommentViewSet)
