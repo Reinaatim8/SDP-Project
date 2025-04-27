08dbac11f4ddefc804f824f2c383de557067fe4e
Reinaatim8 <168911378+Reinaatim8@users.noreply.github.com>
2025-04-27 21:28:40 +0300

    Changed email conditon again

--- Frontend/aits_frontend/src/AITS_Pages/SignUpPage.js
+++ Frontend/aits_frontend/src/AITS_Pages/SignUpPage.js
@@ -105,11 +105,11 @@ const SignUpPage = () => {
           newErrors.email = 'Email has an invalid format';
           toast.warning('Email has an invalid format');
         }
-        if (formData.user_type === 'student' && formData.email && !formData.email('@gmail.com')) {
-          newErrors.email = 'Student email must have @gmail.com or @students.mak.ac.ug';
-        }
-        if (formData.user_type === 'lecturer' && formData.email && !formData.email('@gmail.com') ) {
-          newErrors.email = 'Lecturer email must have @gmail.com with @mak.ac.ug';
+        if (!formData.email) {
+          newErrors.email = 'Email is required';
+        } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+          newErrors.email = 'Email has an invalid format';
+          toast.warning('Email has an invalid format');
         }
         if (!formData.phone_number) newErrors.phone_number = 'Phone number is required';
         break;
