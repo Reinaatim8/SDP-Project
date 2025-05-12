403b2b5052be4a492a23421f63a622619ede47eb
Spire256 <mwanjesimonpeter732@gmail.com>
2025-05-12 18:56:31 +0300

    Updated something

--- issues/forms.py
+++ issues/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser, Issue
 
-# User Registration Forms
+# User Registration Form
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
