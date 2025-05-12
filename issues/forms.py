d8008c10614b1f9436f8755720ab1c9e2c6af7f4
Spire256 <mwanjesimonpeter732@gmail.com>
2025-05-12 18:54:58 +0300

    Removed something

--- issues/forms.py
+++ issues/forms.py
@@ -8,7 +8,7 @@ class CustomUserCreationForm(UserCreationForm):
         model = CustomUser
         fields = ['username', 'first_name', 'last_name', 'email', 'role', 'password']
 
-# Issue FormsS
+# Issue Forms
 class IssueForm(forms.ModelForm):
     class Meta:
         model = Issue
