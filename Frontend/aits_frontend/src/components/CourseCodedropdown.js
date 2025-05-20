533796a3403fef98b198c406340d9f93f758d792
Reinaatim8 <168911378+Reinaatim8@users.noreply.github.com>
2025-05-20 11:27:25 +0300

    added a file for course code dropdown as well

--- Frontend/aits_frontend/src/components/CourseCodedropdown.js
+++ Frontend/aits_frontend/src/components/CourseCodedropdown.js
@@ -1,1 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
+
+const CourseCodedropdown = ({onSelect}) => {
+  const courseCode = ["CSC 1204","CSC 1202","CSC 1104","CSC 1109","CSC 1105","CSC 1100","CSC 1200","CSC 1209"];
+  const [selectedCourseCode, setSelectedCourseCode]= useState('');
+
+  const handleChange = (e) => {
+    setSelectedCourseCode(e.target.value);
+    onSelect(e.target.value);
+  };
+
+  return (
+    <select className="coursename-dropdown" value={selectedCourseCode} onChange={handleChange}>
+      <option value="">Select your Course Unit Code below:</option>
+      {courseCode.map((courseCode, index) => (
+        <option key={index} value={courseCode}>{courseCode}</option>
+      ))}
+    </select>
+  );
+};
+
+export default CourseCodedropdown;
+
