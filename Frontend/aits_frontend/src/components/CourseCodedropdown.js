1c132bd97cc9764f5b02dc9c292acae5ffe0869d
Reinaatim8 <168911378+Reinaatim8@users.noreply.github.com>
2025-05-20 11:42:19 +0300

    Added more course codes in the coursecode dropdown

--- Frontend/aits_frontend/src/components/CourseCodedropdown.js
+++ Frontend/aits_frontend/src/components/CourseCodedropdown.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 
 const CourseCodedropdown = ({onSelect}) => {
-  const courseCode = ["CSC 1204","CSC 1202","CSC 1104","CSC 1109","CSC 1105","CSC 1100","CSC 1200","CSC 1209"];
+  const courseCode = ["CSC 1201","CSC 1202","CSC 1103","CSC 1105","CSC 1106","CSC 1100","CSC 1102","CSK 1101","CSC 1200","CSC 1207","CSC 1204"];
   const [selectedCourseCode, setSelectedCourseCode]= useState('');
 
   const handleChange = (e) => {
